4e67608741193e50bf61faf6730e27b81f2da585
Cammy Hale <cammyhale21@gmail.com>
2024-03-27 00:10:10 +0000

    Fixed some bugs

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,7 +1,5 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
 import "../../styles/home.css";
 
@@ -18,22 +16,10 @@ export const Home = () => {
 	)
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Welcome</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			{store.message && <div className="alert alert-info">
-				{store.message}
-			</div>}
-			<p>
-					<button onClick={() => {
-						if(!tokenCheck) navigate("/login"), alert("First you must log in to see the private page.");
-							else navigate ("/private")
-						}}>
-						Click here to see private page
-					</button>
-			</p>
+		<div className="body">
+			<div className="text-center mt-5">
+				<h1>Welcome</h1>
+			</div>
 		</div>
 	);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -5,25 +5,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			token: null,
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
 			syncTokenFromSessionStore: () => {
 				const token = sessionStorage.getItem("token");
 				if(token && token != "" && token != undefined) setStore({ token: token});
@@ -46,7 +29,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					})
 				};
 				try{
-					const resp = await fetch(`${webURL}/api/signup`, opts)
+					const resp = await fetch(`${webURL}api/signup`, opts)
 					if(resp.status !== 200){
 						alert("There has been some error");
 						return false;
@@ -76,13 +59,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 				};
 
 			try{
-				const resp = await fetch(`${webURL}/api/token`, opts)
+				const resp = await fetch(`${webURL}api/token`, opts)
 				if(resp.status !== 200){
 					alert("There has been some error");
 					return false;
 				} 
-			
-					
 				const data = await resp.json();
 					sessionStorage.setItem("token", data.access_token);
 					setStore({ token: data.access_token})
@@ -100,25 +81,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 						"Authorization": "Bearer " + store.token
 					}
 				};
-				fetch(`${webURL}/api/hello`, opts)
+				fetch(`${webURL}api/hello`, opts)
 					.then(resp => resp.json())
 					.then(data => setStore({ message: data.message }))
 					.catch(error => console.log("Error loading message from backend", error));
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
